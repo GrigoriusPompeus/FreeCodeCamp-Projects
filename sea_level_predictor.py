c3d8cf9e80227f3697477e722cffee3040240040
GrigoriusPompeus <121502735+GrigoriusPompeus@users.noreply.github.com>
2023-01-23 20:31:35 +1000

    Add files via upload

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -18,7 +18,7 @@ def draw_plot():
     predicted_line = linregress(x,y)
     x_pred = pd.Series([i for i in range(1880, 2055)])
     y_pred = predicted_line.slope*x_pred + predicted_line.intercept
-    plt.plot(x_pred, y_pred, 'r')
+    plt.plot(x_pred, y_pred, 'r',label = "Line of best fit using data from 1880")
 
   
     # Create second line of best fit
@@ -30,13 +30,13 @@ def draw_plot():
     second_line = linregress(x_worse,y_worse)
     x2_pred = pd.Series([i for i in range(1880, 2055)])
     y2_pred = second_line.slope *x2_pred + second_line.intercept
-    plt.plot(x2_pred, y2_pred, 'b')
+    plt.plot(x2_pred, y2_pred, 'b',label = "Line of best fit using data from 1930")
 
     # Add labels and title
     ax.set_title('Rise in Sea Level')
     ax.set_xlabel('Year', fontsize = 13)
     ax.set_ylabel('Sea Level (inches)', fontsize = 13)
-    
+    plt.legend()
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
